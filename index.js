b429e0a68cac4ceaae075d06f5312635cd505d9e
Alexey Popov <asdalexey@yandex.ru>
2019-05-22 23:59:37 +0300

    save JSON to file

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 const request = require("request-promise");
 const cheerio = require("cheerio");
+const fs = require("fs");
 
-const URL = "https://www.imdb.com/title/tt0944947/?ref_=vi_close";
+const URLS = [
+  "https://www.imdb.com/title/tt0944947/?ref_=vi_close",
+  "https://www.imdb.com/title/tt4283088/"
+];
 
 (async () => {
-  const response = await request(URL);
-  const $ = cheerio.load(response);
+  const moviesData = [];
+  for (let movie of URLS) {
+    const response = await request({
+      url: movie,
+      headers: {
+        Host: "www.imdb.com",
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        Connection: "keep-alive",
+        "Upgrade-Insecure-Requests": 1,
+        "Cache-Control": "max-age=0",
+        TE: "Trailers"
+      },
+      gzip: true
+    });
+    const $ = cheerio.load(response);
 
-  const title = $("div.title_wrapper>h1").text();
-  const rating = $("span[itemprop='ratingValue']").text();
-  console.log(title, rating);
+    const title = $("div.title_wrapper>h1").text();
+    const rating = $("span[itemprop='ratingValue']").text();
+    const poster = $(".poster img").attr("src");
+    const totalRatings = $(".imdbRating > a > .small").text();
+    const releaseDate = $("a[title='See more release dates']").text();
+    const genres = $(".title_wrapper a[href^='/search/title?genre']")
+      .map((i, el) => $(el).text())
+      .get();
+
+    moviesData.push({
+      title,
+      rating,
+      poster,
+      totalRatings,
+      releaseDate,
+      genres
+    });
+  }
+  fs.writeFileSync("./data.json", JSON.stringify(moviesData), "utf-8");
+  console.log(moviesData);
 })();
